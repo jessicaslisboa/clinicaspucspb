2cf3aa01f27b9e83de9b120de2403cffe71abc2b
jessicaslisboa <jessicaslisboa@gmail.com>
2021-11-29 22:11:37 -0300

    cors

--- src/main/java/com/puc/clinicas/rs/MedicamentoApi.java
+++ src/main/java/com/puc/clinicas/rs/MedicamentoApi.java
@@ -8,6 +8,7 @@ import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.puc.clinicas.repository.MedicamentoRepositorio;
 
 @RestController
 @RequestMapping(value = "/medicamento")
+@CrossOrigin(origins = "*")
 public class MedicamentoApi {
 	
 	@Autowired
